09b7e37ec3885544aef3f501de87767d11969800
Jon Silver <js@jfdi.info>
2020-05-18 21:59:28 +0100

    vastly simplified build & config; fully working

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,10 @@
-ARG S6_ARCH
-FROM oznu/s6-node:12.16.3-${S6_ARCH:-amd64}
+FROM balenalib/raspberrypi3-debian-node:12.16.2-buster-build
 
-RUN apk add --no-cache git python make g++ avahi-compat-libdns_sd avahi-dev dbus \
-    iputils sudo nano \
-  && chmod 4755 /bin/ping \
-  && mkdir /homebridge \
-  && npm set global-style=true \
-  && npm set package-lock=false
-
-RUN case "$(uname -m)" in \
-    x86_64) FFMPEG_ARCH='x86_64';; \
-    armv6l) FFMPEG_ARCH='armv6l';; \
-    armv7l) FFMPEG_ARCH='armv6l';; \
-    aarch64) FFMPEG_ARCH='aarch64';; \
-    *) echo "unsupported architecture"; exit 1 ;; \
-    esac \
-    && set -x \
-    && curl -Lfs https://github.com/oznu/ffmpeg-for-homebridge/releases/download/v0.0.1/ffmpeg-alpine-${FFMPEG_ARCH}.tar.gz | tar xzf - -C / --no-same-owner
-
-ENV HOMEBRIDGE_VERSION=1.0.4
-RUN npm install -g --unsafe-perm homebridge@${HOMEBRIDGE_VERSION}
-
-ENV CONFIG_UI_VERSION=4.19.0 HOMEBRIDGE_CONFIG_UI=0 HOMEBRIDGE_CONFIG_UI_PORT=8080
-RUN npm install -g --unsafe-perm homebridge-config-ui-x@${CONFIG_UI_VERSION}
-
-WORKDIR /homebridge
-VOLUME /homebridge
-
-COPY root /
-
-ARG AVAHI
-ENV ENABLE_AVAHI="${AVAHI:-0}"
+WORKDIR /root/.homebridge
+RUN npm install -g --unsafe-perm homebridge homebridge-mqtt
+COPY ./setup.js ./
+RUN ["node", "./setup.js"]
+RUN ["rm", "./setup.js"]
+ENTRYPOINT [ "homebridge" ]
+# ARG AVAHI
+# ENV ENABLE_AVAHI="${AVAHI:-0}"
---
+++ setup.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+// Generate Homebridge Username
+function generateUsername() {
+    const hexDigits = "0123456789ABCDEF";
+    let username = "0E:";
+    for (let i = 0; i < 5; i++) {
+        username += hexDigits.charAt(Math.round(Math.random() * 15));
+        username += hexDigits.charAt(Math.round(Math.random() * 15));
+        if (i !== 4) {
+            username += ":";
+        }
+    }
+    return username;
+}
+
+// Randomly Select A Port Number
+function selectPort() {
+    if (!isNaN(process.env.HOMEBRIDGE_INIT_PORT)) {
+        const port = parseInt(process.env.HOMEBRIDGE_INIT_PORT);
+        if (port < 65535 && port > 1024) {
+            return port;
+        }
+    }
+    return Math.floor(Math.random() * (54000 - 51000 + 1) + 51000);
+}
+
+const username = generateUsername();
+const port = selectPort();
+const name =
+    "Homebridge " + username.substr(username.length - 5).replace(/:/g, "");
+
+// Base Config
+const config = {
+    bridge: {
+        name: name,
+        username: username,
+        port: port,
+        pin: "031-45-154"
+    },
+    accessories: [],
+    platforms: [
+        {
+            platform: "mqtt",
+            name: "mqtt",
+            url: "mqtt://127.0.0.1"
+        }
+    ]
+};
+
+// Write File
+console.log("Creating default config.json.");
+fs.writeFileSync("./config.json", JSON.stringify(config, null, 4));
+
+// End
+process.exit();
